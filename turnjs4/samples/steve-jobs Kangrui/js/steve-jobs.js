0d9b2981d7f648b6d690867def59b787b03d389f
kangruiw1027 <kwang07@upenn.edu>
2026-02-04 14:21:25 -0500

    Kangrui

--- turnjs4/samples/steve-jobs Kangrui/js/steve-jobs.js
+++ turnjs4/samples/steve-jobs Kangrui/js/steve-jobs.js
@@ -32,7 +32,8 @@ function loadPage(page) {
 
 	$.ajax({url: 'pages/page' + page + '.html'}).
 		done(function(pageHtml) {
-			$('.sj-book .p' + page).html(pageHtml.replace('samples/steve-jobs/', ''));
+			$('.sj-book .p' + page).html(pageHtml.replace('samples/steve-jobs Kangrui Kangrui Wang' +
+				/', ''));
 		});
 
 }
